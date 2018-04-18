99cd37d8ea9ce206119383f8aeb5ab7e28bcf30e
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-18 18:19:27 +0300

    still messing with animation

--- client/src/components/TheHelper/TheHelper.vue
+++ client/src/components/TheHelper/TheHelper.vue
@@ -20,30 +20,11 @@ $size: 80px;
 
 .helper {
   position: absolute;
-  border: 1px solid white;
+  // border: 1px solid white;
   height: $size;
   left: 20px;
   top: 100px;
   width: $size;
   z-index: 10;
-}
-
-.a-enter {
-  animation-name: a-enter;
-  animation-timing-function: ease-in-out;;
-  animation-fill-mode: forwards;
-  animation-duration: .5s;
-}
-
-@keyframes a-enter {
-  0% {
-    transform: translate(-100px, 0);
-  }
-  50% {
-    transform: translate(20px, 0);
-  }
-  100% {
-    transform: translate(0, 0);
-  }
 }
 </style>
--- client/src/components/TheHelper/TheHelperCircle.vue
+++ client/src/components/TheHelper/TheHelperCircle.vue
@@ -1,6 +1,6 @@
 <template>
 <svg class="svg" viewBox="0 0 200 200">
-  <circle class="hover" cx="50%" cy="50%" r="50"/>
+  <circle class="animation" cx="50%" cy="50%" r="50"/>
 </svg>
 </template>
 
@@ -18,23 +18,36 @@ export default {
   width: 100%;
 }
 
-.hover {
-  animation-name: hover;
-  animation-timing-function: ease-in-out;
-  animation-iteration-count: infinite;
-  animation-direction: alternate;
-  animation-duration: 4s;
+// ANIMATION SHORTHAND
+// animation: name duration timing-function delay iteration-count direction fill-mode play-state;
+
+.animation {
+  animation:
+    a-hover 12s ease-in-out infinite alternate;
+    // hoverHorizontal 11s ease-in-out infinite alternate;
 }
 
-@keyframes hover {
+@keyframes a-hover {
   0% {
-    transform: translate(0, 0);
+    transform: translate(0px, 0px);
+  }
+  15% {
+    transform: translate(2px, 15px);
+  }
+  30% {
+    transform: translate(3px, 0px);
   }
   50% {
-    transform: translate(0, 10px);
+    transform: translate(1px, 20px);
+  }
+  65% {
+    transform: translate(-2px, 0px);
+  }
+  85% {
+    transform: translate(0px, 17px);
   }
   100% {
-    transform: translate(0, 0);
+    transform: translate(0px, 0px);
   }
 }
 </style>
