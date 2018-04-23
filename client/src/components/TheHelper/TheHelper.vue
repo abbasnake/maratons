59c07e808177e04fb3081e7a2aa43a070c3b99e1
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-23 08:54:48 +0300

    trying to design a good environment for js animation

--- client/src/components/TheHelper/TheHelper.vue
+++ client/src/components/TheHelper/TheHelper.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="helper animation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+  <svg class="helper" :style="styleObject" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
     <helper-circle></helper-circle>
   </svg>
 </template>
@@ -11,6 +11,35 @@ export default {
   name: 'TheHelper',
   components: {
     'helper-circle': TheHelperCircle
+  },
+  data () {
+    return {
+      xPos: 20,
+      yPos: 100
+    }
+  },
+  mounted () {
+    this.loop()
+  },
+  computed: {
+    styleObject () {
+      return {
+        'transform': `translate(${this.xPos}px, ${this.yPos}px)`
+      }
+    }
+  },
+  methods: {
+    changeContentAnimation () {
+
+    },
+    mainAnimation () {
+      requestAnimationFrame(this.mainAnimation)
+      // if (this.hoverAnimationOn) { this.hoverAnimation() }
+      this.changeContentAnimation()
+    },
+    loop () {
+      requestAnimationFrame(this.mainAnimation)
+    }
   }
 }
 </script>
@@ -19,29 +48,12 @@ export default {
 $size: 80px;
 
 .helper {
-  // border: 1px solid white;
+  border: 1px solid white;
   position: absolute;
   height: $size;
-  left: 20px;
-  top: 100px;
+  // left: 20px; // doing this in javascript
+  // top: 100px; // doing this in javascript
   width: $size;
   z-index: 10;
 }
-
-// .animation {
-//   animation:
-//       a-get-stuff 1s ease-in-out alternate;
-// }
-
-// @keyframes a-get-stuff {
-//   0% {
-//     transform: translate(0px, 0px);
-//   }
-//   50% {
-//     transform: translate(4000px, 300px);
-//   }
-//   100% {
-//     transform: translate(0px, 0px);
-//   }
-// }
 </style>
